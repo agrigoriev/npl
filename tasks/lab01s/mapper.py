f474c2bc6fb082f86232c5c12665f8d7b52f71a2
Alexey Grigoriev <agrigoriev@gmail.com>
2017-03-22 18:56:56 +0300

    Initial commit

---
+++ tasks/lab01s/mapper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+def do_map(record):
+    data = record.split(",")
+    yield data[2].strip(), data[4].strip()
+
+
+for line in sys.stdin:
+    for key, value in do_map(line):
+        print key + "\t" + value
