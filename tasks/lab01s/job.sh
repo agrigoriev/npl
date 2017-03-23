f206bb98243c5eafcb7a06396dabafe6f14954d2
Alexey Grigoriev <a.grigoriev@innopolis.ru>
2017-03-23 12:02:47 +0300

    changed input directory

--- tasks/lab01s/job.sh
+++ tasks/lab01s/job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 hadoop jar /usr/hdp/2.5.3.0-37/hadoop-mapreduce/hadoop-streaming.jar \
 -D mapred.reduce.tasks=1 \
--input /users/adverts \
+-input /users/advert \
 -file /home/hdfs/npl/tasks/lab01s/mapper.py -mapper mapper.py \
 -file /home/hdfs/npl/tasks/lab01s/reducer.py -reducer reducer.py \
 -output /users/adverts
