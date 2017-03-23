f4574f656ac48a954dbb39246d94ca5390fcaf9d
Alexey Grigoriev <a.grigoriev@innopolis.ru>
2017-03-23 11:13:46 +0300

    output directory change

--- tasks/lab01s/job.sh
+++ tasks/lab01s/job.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-hadoop fs -rm /users/adverts/result
 hadoop jar /usr/hdp/2.5.3.0-37/hadoop-mapreduce/hadoop-streaming.jar \
 -D mapred.reduce.tasks=1 \
 -input /users/adverts \
 -file /home/hdfs/npl/tasks/lab01s/mapper.py -mapper mapper.py \
--file /home/hdfs/npl/tasks/lab01s/reducer.py -reducer reducer.py
--output /users/adverts/result \
+-file /home/hdfs/npl/tasks/lab01s/reducer.py -reducer reducer.py \
+-output /users/adverts
--- tasks/lab01s/test.sh
+++ tasks/lab01s/test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source activate python2
 cd ~/npl/tasks/lab01s/
 tail -n 10000 ~/npl/bigdata/advert.log | ./mapper.py | sort | ./reducer.py > result
 echo "Done!"
