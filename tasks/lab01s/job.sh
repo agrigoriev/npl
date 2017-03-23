bae0a7ac981c82c86152f8eecef71768f3a48f9f
Alexey Grigoriev <a.grigoriev@innopolis.ru>
2017-03-23 11:06:21 +0300

    remove result before executing

--- tasks/lab01s/job.sh
+++ tasks/lab01s/job.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+hadoop fs -rm /users/adverts/result
 hadoop jar /usr/hdp/2.5.3.0-37/hadoop-mapreduce/hadoop-streaming.jar \
-  -D mapred.reduce.tasks=1 \
-  -input /users/adverts \
-  -output /users/adverts/result \
-  -mapper "mapper.py" \
-  -reducer "reducer.py"
+-D mapred.reduce.tasks=1 \
+-input /users/adverts \
+-file /home/hdfs/npl/tasks/lab01s/mapper.py -mapper mapper.py \
+-file /home/hdfs/npl/tasks/lab01s/reducer.py -reducer reducer.py
+-output /users/adverts/result \
