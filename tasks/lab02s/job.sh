9ed1899ebf55aedbaf0d98df923dbcdae21bb7ba
Alexey Grigoriev <agrigoriev@gmail.com>
2017-04-02 18:33:14 +0300

    cluster changes

--- tasks/lab02s/job.sh
+++ tasks/lab02s/job.sh
@@ -2,5 +2,5 @@
 hadoop jar /usr/hdp/2.5.3.0-37/hadoop-mapreduce/hadoop-streaming.jar \
 -input "/labs/facetz_2015_02_12" \
 -file ~/npl/tasks/lab02s/mapper.py -mapper mapper.py \
--file ~/npl/tasks/lab02s/reducer.py -mapper reducer.py \
+-file ~/npl/tasks/lab02s/reducer.py -reducer reducer.py \
 -output "/user/alexey.grigoriev/top350"
